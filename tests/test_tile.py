40791456d0c1ee80cd20be997986e33df86f5abc
Murthy10 <samuel.kurath@gmail.com>
2017-04-19 10:44:38 +0200

    added some cross checks

--- tests/test_tile.py
+++ tests/test_tile.py
@@ -7,10 +7,25 @@ def tms():
     return 67, 83
 
 
-def test_from_google(tms):
-    google_x, google_y = 67, 44
+@pytest.fixture(scope='module')
+def google():
+    return 67, 44
 
-    tile = Tile.from_google(google_x=google_x, google_y=google_y, zoom=7)
+
+@pytest.fixture(scope='module')
+def quad_tree():
+    return '1202211'
+
+
+@pytest.fixture(scope='module')
+def zoom():
+    return 7
+
+
+def test_from_google(tms, google, zoom):
+    google_x, google_y = google
+
+    tile = Tile.from_google(google_x=google_x, google_y=google_y, zoom=zoom)
     print(tile.tms)
     assert tile.tms == tms
 
@@ -24,9 +39,24 @@ def test_from_google_1():
     assert tile.tms == tms_tasmania
 
 
-def test_from_tms(tms):
+def test_from_tms(tms, zoom):
     tms_x, tms_y = tms
 
-    tile = Tile.from_tms(tms_x=tms_x, tms_y=tms_y, zoom=7)
+    tile = Tile.from_tms(tms_x=tms_x, tms_y=tms_y, zoom=zoom)
 
     assert tile.tms == tms
+
+
+def test_from_quad_tree(tms, quad_tree, zoom):
+    tile = Tile.from_quad_tree(quad_tree=quad_tree)
+
+    assert tile.tms == tms
+    assert tile.zoom == zoom
+
+
+def test_cross_check(tms, google, quad_tree, zoom):
+    tile = Tile.from_quad_tree(quad_tree=quad_tree)
+
+    assert tile.tms == tms
+    assert tile.zoom == zoom
+    assert tile.google == google
