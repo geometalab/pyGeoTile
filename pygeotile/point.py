4e8c33caf7b332941cba175e0be7025457696901
Murthy10 <samuel.kurath@gmail.com>
2017-04-19 08:05:53 +0200

    point nearly finished

--- pygeotile/point.py
+++ pygeotile/point.py
@@ -2,42 +2,41 @@ import math
 
 
 class Point:
-    def __init__(self, tile_size=256, earth_radius=6378137):
+    def __init__(self, tile_size=256, earth_radius=6378137, zoom=None):
         super().__init__()
         self._latitude = None
         self._longitude = None
-        self._zoom = None
+        self._zoom = zoom
         self._tile_size = tile_size
         self._earth_radius = earth_radius
 
     @classmethod
-    def from_coordinates(cls, latitude=0.0, longitude=0.0):
-        point = cls()
-        point.coordinates = (latitude, longitude)
+    def from_coordinates(cls, latitude=0.0, longitude=0.0, zoom=None):
+        point = cls(zoom=zoom)
+        point.latitude_longitude = (latitude, longitude)
         return point
 
     @classmethod
-    def from_pixel(cls, pixel_x=0, pixel_y=0, zoom=0):
-        point = cls()
-        point.zoom = zoom
+    def from_pixel(cls, pixel_x=0, pixel_y=0, zoom=None):
+        point = cls(zoom=zoom)
         meter_x = pixel_x * point.resolution - point.origin_shift
         meter_y = pixel_y * point.resolution - point.origin_shift
-        return point.from_meters(meter_x=meter_x, meter_y=meter_y)
+        return point.from_meters(meter_x=meter_x, meter_y=meter_y, zoom=zoom)
 
     @classmethod
-    def from_meters(cls, meter_x=0, meter_y=0.0):
-        point = cls()
+    def from_meters(cls, meter_x=0, meter_y=0.0, zoom=None):
+        point = cls(zoom=zoom)
         longitude = (meter_x / point.origin_shift) * 180.0
         latitude = (meter_y / point.origin_shift) * 180.0
         latitude = 180 / math.pi * (2 * math.atan(math.exp(latitude * math.pi / 180.0)) - math.pi / 2.0)
-        return point.from_coordinates(latitude=latitude, longitude=longitude)
+        return point.from_coordinates(latitude=latitude, longitude=longitude, zoom=zoom)
 
     @property
-    def coordinates(self):
+    def latitude_longitude(self):
         return self._latitude, self._longitude
 
-    @coordinates.setter
-    def coordinates(self, value):
+    @latitude_longitude.setter
+    def latitude_longitude(self, value):
         if type(value) is tuple:
             latitude, longitude = value
             self._latitude = latitude
@@ -48,7 +47,7 @@ class Point:
     @property
     def pixels(self):
         """Gets pixels of the EPSG:4326 pyramid by a specific zoom"""
-        latitude, longitude = self.coordinates
+        latitude, longitude = self.latitude_longitude
         pixel_x = (180 + latitude) / self.resolution
         pixel_y = (90 + longitude) / self.resolution
         return pixel_x, pixel_y
@@ -59,7 +58,7 @@ class Point:
     @property
     def meters(self):
         """Converts given lat/lon in WGS84 Datum to XY in Spherical Mercator EPSG:900913"""
-        latitude, longitude = self.coordinates
+        latitude, longitude = self.latitude_longitude
         meter_x = longitude * self.origin_shift / 180.0
         meter_y = math.log(math.tan((90 + longitude) * math.pi / 360.0)) / (math.pi / 180.0)
         meter_y = meter_y * self.origin_shift / 180.0
@@ -67,7 +66,6 @@ class Point:
 
     @property
     def zoom(self):
-        # TODO Attribute Error or something similar if zoom is None
         if not self._zoom:
             raise TypeError('Zoom is not set!')
         return self._zoom
--- tests/test_point.py
+++ tests/test_point.py
@@ -12,7 +12,7 @@ def test_from_coordinates(coordinates):
     latitude, longitude = coordinates
     point = Point.from_coordinates(latitude=latitude, longitude=longitude)
 
-    assert point.coordinates == coordinates
+    assert point.latitude_longitude == coordinates
 
 
 def test_from_meters(coordinates):
@@ -20,18 +20,44 @@ def test_from_meters(coordinates):
     meter_y = 5942074.07
 
     point = Point.from_meters(meter_x=meter_x, meter_y=meter_y)
-    latitude, longitude = point.coordinates
+    latitude, longitude = point.latitude_longitude
 
     assert pytest.approx(latitude, 0.01) == coordinates[0]
     assert pytest.approx(longitude, 0.01) == coordinates[1]
 
 
-def test_from_pixel(coordinates):
-    meter_x = 890555.93
-    meter_y = 5942074.07
+def test_from_pixel():
+    latitude, longitude = 41.85, -87.65
+    pixel_x, pixel_y = (34430575, 49899071)
+
+    point = Point.from_pixel(pixel_y=pixel_y, pixel_x=pixel_x, zoom=19)
+    print(point.latitude_longitude)
+    print(point.pixels)
+    print(point.meters)
+
+    assert pytest.approx(point.latitude_longitude[0], 0.2) == latitude
+    assert pytest.approx(point.latitude_longitude[1], 0.2) == longitude
+
 
 def test_zoom_exception():
     point = Point()
     with pytest.raises(Exception) as exception:
         _ = point.zoom
     assert 'Zoom is not set!' in str(exception.value)
+
+
+'''
+Chicago, IL
+LatLng: (41.85, -87.64999999999998)
+Zoom level: 19
+World Coordinate: (65.67111111111113, 95.17492654697409)
+Pixel Coordinate: (34430575, 49899071)
+Tile Coordinate: (134494, 194918)
+'''
+
+'''
+mx: 4658720.69
+my: -24790661.63
+
+LatLng: 41.85, -87.65
+'''
